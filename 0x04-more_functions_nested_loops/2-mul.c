c6680d4b7c52c3e278abf56c974885f9e4ea7ef1
Mokoena13 <ernestmokoena.mokoena@gmail.com>
2023-03-23 11:17:12 -0700

    m

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,32 +1,14 @@
 #include "main.h"
-
 /**
- * mul - multiplies two integers.
- * @a: first number.
- * @b: second number.
- * Return: multiplication.
+ * mul - checks for checks for a digit (0 through 9).
+ * @a: a - Variable
+ * @b: b - variable
+ * Return: Always 0.
  */
+
 int mul(int a, int b)
 {
-	return (a * b);
-}
-
-
-Task 3. 3-print_numbers.c
-
-#include "main.h"
-
-/**
- * print_numbers - prints numbers between 0 to 9.
- * Return: no return.
- */
-void print_numbers(void)
-{
-	int ch;
-
-	for (ch = 48; ch < 58; ch++)
-	{
-		_putchar(ch);
-	}
-	_putchar('\n');
+	 int mul;
+	 mul = a * b;
+	 return (mul);
 }
