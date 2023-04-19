e9523d96870c850f962c2fd23ebeba0561b3eb32
Mokoena13 <ernestmokoena.mokoena@gmail.com>
2023-04-19 10:36:08 -0700

    Function Pointers

--- 0-print_name.c
+++ 0-print_name.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
- * print_name - print name using pointer to func
+ * print_name - print name using pointer to function
  * @name: string to add
- * @f: pointer to func
+ * @f: pointer to function
  * Return: nothing
  **/
 
@@ -15,3 +15,4 @@ void print_name(char *name, void (*f)(char *))
 
 	f(name);
 }
+
